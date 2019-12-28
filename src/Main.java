32d5b0a96a78fa115b8d223341beb54fd10cb63d
Roman Velikorod <romavelik@gmail.com>
2019-12-28 11:21:23 +0200

    merge sort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        RecursiveAlgorithms.towersOfHanoi(3, 'A', 'B', 'C');
+        int [] array = {8, 5, 2, 6, 77, 55};
+        Sort.mergeSort(array);
+        for(int i : array){
+            System.out.println(i);
+        }
     }
 }
--- src/Sort.java
+++ src/Sort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Sort {
     public static void bubbleSort(int[] array){
         int temp;
@@ -13,7 +15,40 @@ public class Sort {
             }
         }
     }
-    public static void mergeSort(){}
+    public static void mergeSort(int[] array){
+        int size = array.length;
+        if(size<2){
+            return;
+        }else{
+            int mid = size/2;
+            int[] left = Arrays.copyOfRange(array, 0, mid);
+            int[] right = Arrays.copyOfRange(array, mid, size);
+            mergeSort(left);
+            mergeSort(right);
+            merge(array, left, right);
+        }
+    }
+
+    private static void merge(int[] array, int[] left, int[] right) {
+        int resultIndex = 0;
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int leftSize = left.length;
+        int rightSize = right.length;
+        while (leftIndex < leftSize && rightIndex < rightSize){
+            if (left[leftIndex] <= right[rightIndex]) {
+                array [resultIndex++] = left [leftIndex++];
+            }else{
+                array[resultIndex++] = right[rightIndex++];
+            }
+        }
+        while (leftIndex < leftSize){
+            array[resultIndex++] = left [leftIndex++];
+        }
+        while (rightIndex < rightSize){
+            array[resultIndex++] = right[rightIndex++];
+        }
+    }
 
     public static void quickSort(){}
 }
